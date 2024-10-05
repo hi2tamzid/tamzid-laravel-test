1cd782bce023ee333810a02a485c28f1ec7f9dc7
Tamzid Mahmud <tamzidmahmud2@gmail.com>
2024-10-05 23:35:45 +0600

    feature: routes created

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [IndexController::class, 'index'])->name('index');
+Route::get('/data', [IndexController::class, 'showData'])->name('data.show');
+Route::post('/form-submit', [IndexController::class, 'submitForm'])->name('form.submit');
+Route::post('/data-update', [IndexController::class, 'updateData'])->name('data.update');
